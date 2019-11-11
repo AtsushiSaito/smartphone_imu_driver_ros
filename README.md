af6b2c39be2ac4988ac08d12bada07d897e7709c
AtsushiSaito <atsushi31100854@gmail.com>
2019-11-11 09:44:30 +0900

    Update

--- README.md
+++ README.md
@@ -6,12 +6,10 @@ The smartphone will be the IMU sensor.
 Web経由でデータをやり取りするため、Rosbridgeを使用しています。
 これにより、特別なアプリなどを使用せず、Webページにアクセスするだけで簡単に使えます。
 
-## 出力形式
-ROSの"sensor_msgs/Imu"に準拠した出力になります。
+## 使用可能端末
+Android(加速度センサやジャイロセンサが搭載されている端末のみ)
 
-加速度 -> x,y,z[m/s2]
-
-角加速度 -> x,y,z[rad/s]
+※ iOSでは現状動作しません。(近日対応予定)
 
 ## インストール
 
@@ -27,13 +25,27 @@ cd ~/catkin_ws && catkin_make && source ~/catkin_ws/devel/setup.bash
 ```
 
 ## 使用方法
-以下のコマンドを実行し、Webサーバを立ち上げます。
+以下のコマンドで`rosbridge`を立ち上げます。
 ```
-roslaunch ros_smartphone_imu sample.launch
+roslaunch ros_smartphone_imu secure_rosbridge.launch
 ```
-ROSが起動しているPCのIPアドレスを調べ、スマートフォンのブラウザから以下のようにアクセスします。
+同時にrvizを表示する場合
 ```
-IPアドレス:8000
-# 例 -> 192.168.0.33:8000
+roslaunch ros_smartphone_imu secure_rosbridge_rviz.launch
 ```
-これで"imu/data_raw"というTopicが配信されるようになります。
+
+## ngrokでrosbridgeをSSL接続
+
+ngrokをインストールし、以下のコマンドを実行します。
+`192.168.xxx.xxx:9000`の部分は、`rosbridge`が起動しているPCのIPアドレスとポート番号に置き換えます。
+```
+./ngrok http 192.168.xxx.xxx:9000
+```
+
+コマンドを実行すると、以下のような画面が表示されるので、`http://xxxxxxxxxxx.ngrok.io`の`xxxxxxxxxxx.ngrok.io`をメモしておきます。
+
+[https://atsushisaito.github.io/ros_smartphone_imu/](https://atsushisaito.github.io/ros_smartphone_imu/)にアクセスします。
+
+接続先に先程メモした``xxxxxxxxxxx.ngrok.io`を入力し、`接続`ボタンを押します。
+
+接続が成功すると、"imu/data_raw"というTopicが配信されるようになります。
--- docs/main.js
+++ docs/main.js
@@ -1,41 +1,133 @@
+// 構造体
+function ImuData() {
+    this.a_x;
+    this.a_y;
+    this.a_z;
+    this.v_a;
+    this.v_b;
+    this.v_g;
+
+    this.o_x;
+    this.o_y;
+    this.o_z;
+    this.o_w;
+}
+
+var imu_data = new ImuData();
+quaternionpose = new THREE.Quaternion;
+var imu_publisher;
+
 function connect() {
     if (address_form.address.value == "") {
         alert("IPアドレスを入力してください。");
     } else {
-        // alert("IP:" + address_form.address.value);
-        var ros = new ROSLIB.Ros({ url: 'wss://' + address_form.address.value + ':9000' });
+        var ros = new ROSLIB.Ros({ url: 'wss://' + address_form.address.value });
 
-        var ros_pub = new ROSLIB.Topic({
+        imu_publisher = new ROSLIB.Topic({
             ros: ros,
             name: 'imu/data_raw',
             messageType: "sensor_msgs/Imu"
         });
 
-        ros.on('connection', function () { console.log("WebSocket: connected"); });
-        ros.on('error', function (error) { console.log("WebSocket error: ", error); });
-        ros.on('close', function () { console.log("WebSocket: closed"); });
+        ros.on('connection', function () {
+            console.log("WebSocket: connected");
+            document.getElementById("wss_info").innerHTML = "接続済み";
+        });
+        ros.on('error', function (error) {
+            console.log("WebSocket error: ", error);
+            document.getElementById("wss_info").innerHTML = "接続エラー";
+        });
+        ros.on('close', function () {
+            console.log("WebSocket: closed");
+            document.getElementById("wss_info").innerHTML = "接続切断済み";
+        });
+    }
+}
+
+function request_permission() {
+    if (
+        DeviceMotionEvent &&
+        DeviceMotionEvent.requestPermission &&
+        typeof DeviceMotionEvent.requestPermission === 'function'
+    ) {
+        DeviceMotionEvent.requestPermission();
+    }
+    if (
+        DeviceOrientationEvent &&
+        DeviceOrientationEvent.requestPermission &&
+        typeof DeviceOrientationEvent.requestPermission === 'function'
+    ) {
+        DeviceOrientationEvent.requestPermission();
+    }
+}
+
+window.addEventListener("deviceorientation", (event) => {
+    // http://wiki.ros.org/roslibjs/Tutorials/Publishing%20video%20and%20IMU%20data%20with%20roslibjs
+    var beta_radian = ((event.beta + 360) / 360 * 2 * Math.PI) % (2 * Math.PI);  // x軸（左右）まわりの回転の角度（引き起こすとプラス）
+    var gamma_radian = ((event.gamma + 360) / 360 * 2 * Math.PI) % (2 * Math.PI);// y軸（上下）まわりの回転の角度（右に傾けるとプラス）
+    var alpha_radian = ((event.alpha + 360) / 360 * 2 * Math.PI) % (2 * Math.PI);// z軸（表裏）まわりの回転の角度（反時計回りがプラス）
+    var eurlerpose = new THREE.Euler(beta_radian, gamma_radian, alpha_radian);
+    quaternionpose.setFromEuler(eurlerpose);
+});
+
+var data_send = function () {
+    if (imu_publisher != null) {
+        var msg = new ROSLIB.Message({
+            header: {
+                frame_id: "imu_link"
+            },
+            orientation: {
+                x: quaternionpose.x,
+                y: quaternionpose.y,
+                z: quaternionpose.z,
+                w: quaternionpose.w
+            },
+            orientation_covariance: [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            angular_velocity: {
+                x: imu_data.v_b,
+                y: imu_data.v_g,
+                z: imu_data.v_a,
+            },
+            angular_velocity_covariance: [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            linear_acceleration: {
+                x: imu_data.a_x,
+                y: imu_data.a_y,
+                z: imu_data.a_z,
+            },
+            linear_acceleration_covariance: [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        });
+        imu_publisher.publish(msg);
     }
 }
 
 window.addEventListener("devicemotion", devicemotionHandler);
 function devicemotionHandler(event) {
-    accel_x = event.accelerationIncludingGravity.x;
-    accel_y = event.accelerationIncludingGravity.y;
-    accel_z = event.accelerationIncludingGravity.z;
-    alpha = 2 * 3.141592 * (event.rotationRate.alpha / 360);
-    beta = 2 * 3.141592 * (event.rotationRate.beta / 360);
-    gamma = 2 * 3.141592 * (event.rotationRate.gamma / 360);
 
-    document.getElementById("a_x").innerHTML = accel_x;
-    document.getElementById("a_y").innerHTML = accel_y;
-    document.getElementById("a_z").innerHTML = accel_z;
-    document.getElementById("l_b").innerHTML = beta;
-    document.getElementById("l_g").innerHTML = gamma;
-    document.getElementById("l_a").innerHTML = alpha;
+    if (event.accelerationIncludingGravity.x != null) {
+        imu_data.a_x = event.accelerationIncludingGravity.x;
+        imu_data.a_y = event.accelerationIncludingGravity.y;
+        imu_data.a_z = event.accelerationIncludingGravity.z;
+        document.getElementById("a_x").innerHTML = imu_data.a_x;
+        document.getElementById("a_y").innerHTML = imu_data.a_y;
+        document.getElementById("a_z").innerHTML = imu_data.a_z;
+    } else {
+        document.getElementById("a_x").innerHTML = "NULL";
+        document.getElementById("a_y").innerHTML = "NULL";
+        document.getElementById("a_z").innerHTML = "NULL";
+    }
 
-    var msg = new ROSLIB.Message({
-        "angular_velocity": { "x": accel_x, "y": accel_y, "z": accel_z },
-        "linear_acceleration": { "x": beta, "y": gamma, "z": alpha }
-    });
-    ros_pub.publish(msg);
+    if (event.rotationRate.alpha != null) {
+        imu_data.v_g = event.rotationRate.gamma;
+        imu_data.v_b = event.rotationRate.beta;
+        imu_data.v_a = event.rotationRate.alpha;
+        document.getElementById("l_b").innerHTML = imu_data.v_b;
+        document.getElementById("l_g").innerHTML = imu_data.v_g;
+        document.getElementById("l_a").innerHTML = imu_data.v_a;
+    } else {
+        document.getElementById("l_b").innerHTML = "NULL";
+        document.getElementById("l_g").innerHTML = "NULL";
+        document.getElementById("l_a").innerHTML = "NULL";
+    }
 };
+
+setInterval(data_send, 10);
